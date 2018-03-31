1d77416fb7d9d71fe1a66c62d523da0be189784e
SevenOutman <leishenghao@126.com>
2018-04-01 01:11:14 +0800

    diff() api

--- src/Store.js
+++ src/Store.js
@@ -34,19 +34,49 @@ class Store {
     return !_isEqual(this._oldValues, this._values);
   }
 
+  diff() {
+    if (!this.hasChanged()) {
+      return null
+    }
+
+    let diff = {
+      added: {},
+      changed: {},
+      removed: {}
+    }
+    Object.keys(this._values).forEach(key => {
+      if (!this._oldValues.hasOwnProperty(key)) {
+        diff.added[key] = { value: this._values[key] }
+      } else if (!_isEqual(this._values[key], this._oldValues[key])) {
+        diff.changed[key] = { value: this._values[key], oldValue: this._oldValues[key] }
+      }
+    })
+
+    Object.keys(this._oldValues).forEach(key => {
+      if (!this._values.hasOwnProperty(key)) {
+        diff.removed[key] = { oldValue: this._oldValues[key] }
+      }
+    })
+    return diff
+  }
+
   async flush() {
     if (!this.hasChanged()) {
       return this;
     }
 
-    return this._gistApi
+    await this._gistApi
       .update({
         files: {
           [storeFileName(this._name)]: {
-            content: JSON.stringify(this._oldValues),
+            content: JSON.stringify(this._values),
           },
         },
       });
+
+    this._oldValues = { ...this._values }
+
+    return this
   }
 }
 
